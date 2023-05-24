124fe959b52999e61881e05c72db0c02e827d34a
KitnissPrior <kittrisprior@gmail.com>
2023-05-24 09:39:50 +0500

    boss message added

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message : MonoBehaviour
+{
+    public GameObject MessageWindow;
+
+    void Start()
+    {
+        MessageWindow.SetActive(false);
+    }
+
+    public void CloseMessage()
+    {
+        MessageWindow.SetActive(false);
+        ResumeGame();
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Character>(out Character character))
+        {
+            Physics2D.IgnoreCollision(character.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+            PauseGame();
+            MessageWindow.SetActive(true);
+        }
+    }
+}

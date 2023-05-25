c78f8085c50607ed3802e94017d82a2f702724c8
KitnissPrior <kittrisprior@gmail.com>
2023-05-25 17:51:42 +0500

    start+chest messages

--- Assets/Scripts/Message.cs
+++ Assets/Scripts/Message.cs
@@ -1,20 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Message : MonoBehaviour
 {
     public GameObject MessageWindow;
+    public TMP_Text CurrentText;
+
+    public enum MessageType
+    {
+        Start,
+        TakeShield,
+        Boss
+    };
+
+    public MessageType CurrentType;
+
+    public Dictionary<MessageType, string[]> Messages;
+
+    private int _clickCount = 0;
+
+    private string[] _startMessages =
+    {
+        "Ты попал в логово котов — настоящий лабиринт, полный опасностей.\n"+
+        "Чтобы выбраться, тебе предстоит пройти его, обходя ловушки, собирая бонусы и сражаясь с котами.",
+        "Чтобы управлять мышонком, используй стрелки или клавиши W, A, S, D",
+        "Количество собранных бонусов ты можешь увидеть в инвентаре справа.\nКстати о бонусах. Посмотри, какой сундук! Откроем его?",
+    };
+
+    private string[] _takeShieldMessages =
+    {
+        "Тебе выпал щит. Он сможет ненадолго защитить тебя от ловушек и врагов, но при контакте с кошачьим клубком щит разрушится.",
+        "Чтобы использовать щит, кликни на соответствующую картинку в инвентаре справа или нажми клавишу \"3\""
+    };
+
+    private string[] _bossMessages =
+    {
+        "Ты подошел к комнате босса. За ней находится выход наружу.\n"+
+        "Но чтобы открыть последнюю дверь, надо сразиться с боссом и получить его ключ.\n"+
+        "Будь осторожен! Как только ты войдешь в следующую комнату, все двери в ней закроются и назад пути не будет!\n"+
+        "Желаем удачи!",
+    };
+
+    private void FillMessageDictionary()
+    {
+        Messages = new Dictionary<MessageType, string[]>();
+
+        Messages.Add(MessageType.Start, _startMessages);
+        Messages.Add(MessageType.TakeShield, _takeShieldMessages);
+        Messages.Add(MessageType.Boss, _bossMessages);
+    }
 
     void Start()
     {
         MessageWindow.SetActive(false);
+
+        FillMessageDictionary();
     }
 
-    public void CloseMessage()
+    private void CloseMessage()
     {
         MessageWindow.SetActive(false);
         ResumeGame();
+    }
+
+    public void OnButtonClick()
+    {
+        _clickCount++;
+
+        if (_clickCount < Messages[CurrentType].Length)
+        {
+            CurrentText.text = Messages[CurrentType][_clickCount];
+        }
+        else
+        {
+            CloseMessage();
+        }
     }
 
     void PauseGame()
